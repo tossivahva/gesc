8671132d8a9b4b710912f195c52d86de2927c62c
tossivahva <tossivahva@gmail.com>
2023-11-24 19:10:14 +0300

    Intialized project. Created raw sketch template for Nav

---
+++ app/ui/navbar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Navbar() {
+   return (
+      <nav className='fixed flex w-full h-20 bg-[#0d4c84] backdrop-blur-lg place-content-center border-b-cyan-700 border-b'>
+         <div className='flex justify-between items-center w-full h-full px-4 max-w-screen-xl'>
+            <Link href='/'>
+               <Image src='gesc-logo-light.svg'
+                      alt='GES Computer Logo'
+                      width={220}
+                      height={50}
+               />
+            </Link>
+            <div>
+               <ul className='hidden sm:flex text-white'>
+                  <Link href='#'>
+                     <li className='ml-10 text-lg'>О нас</li>
+                  </Link>
+                  <Link href='#'>
+                     <li className='ml-10 text-lg'>Продукция</li>
+                  </Link>
+                  <Link href='#'>
+                     <li className='ml-10 text-lg'>Гарантия</li>
+                  </Link>
+                  <Link href='#'>
+                     <li className='ml-10 text-lg'>Контакты</li>
+                  </Link>
+               </ul>
+            </div>
+            <div>
+               <button
+                  className='bg-orange-500 hover:bg-orange-600 text-lg text-white font-medium py-2 px-4 rounded-lg'>
+                  Стать партнером
+               </button>
+            </div>
+         </div>
+      </nav>
+   );
+};
